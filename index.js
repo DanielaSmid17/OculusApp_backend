96a8d7676d28420a3a2bb12f2efe0cec472bc17c
DanielaSmid17 <dani.smid@gmail.com>
2021-01-20 12:04:03 +0200

    database connected

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config();
+const { initializeDB } = require('./db')
 
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+
+initializeDB()
 
 const port = process.env.PORT;
 app.listen(port, () => {
